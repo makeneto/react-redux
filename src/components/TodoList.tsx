2d6c8b195980570f91e792b01221231befa3c212
Makene Neto <makeneto17@gmail.com>
2025-10-15 15:47:02 +0100

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import { useAppSelector } from "../store"
+
+export default function TodoList() {
+    const todos = useAppSelector((store) => {
+        return store.todo
+    })
+
+    console.log(todos)
+
+    return (
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo}>{todo}</li>
+            ))}
+        </ul>
+    )
+}
---
+++ src/store/index.ts
@@ -0,0 +1,25 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit"
+import { useSelector } from "react-redux"
+import type { TypedUseSelectorHook } from "react-redux"
+
+const todoSlice = createSlice({
+    name: "todo",
+    initialState: ["comer bem", "treinar"] as string[],
+
+    reducers: {
+        add(state, action) {
+            state.push(action.payload.newTodo)
+        },
+    },
+})
+
+export const store = configureStore({
+    reducer: {
+        todo: todoSlice.reducer,
+    },
+})
+
+export const { add } = todoSlice.actions
+
+export type RootState = ReturnType<typeof store.getState>
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
